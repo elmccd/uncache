65c85a3d48b7519edb22c0decd232715dc34c30d
Maciej Dudziński <elmccd@gmail.com>
2014-06-30 17:48:26 +0200

    coverage

--- test/test.js
+++ test/test.js
@@ -23,15 +23,78 @@ function getExpected(filePath) {
 }
 
 function compare(name, expectedName, done) {
-    assert.equal(uncache(String(getFixture(name).contents), {
+    assert.equal(uncache.init(String(getFixture(name).contents), {
             srcDir: path.join('test', 'fixtures'),
             distDir: path.join('test', 'expected')
         }),
         String(getExpected(expectedName).contents));
     done();
-
-
 }
+
+describe('extractTags', function () {
+    it('should parse script and link tags', function (done) {
+        var raw = '<script></script><script></script><link />'
+        var tags = uncache.extractTags(raw);
+        assert.equal(tags.length, 3);
+        done();
+    });
+});
+
+describe('get file name with append:time', function () {
+    it('should return changed filename', function (done) {
+        var config = {
+            append: 'time',
+            rename: false
+        };
+        var newFileName = uncache.getNewFileName('myFile.js', config);
+        assert.equal(newFileName, 'myFile.js?' + Math.round(+new Date() / 1000));
+        done();
+    });
+});
+
+describe('parse line without src or html', function () {
+    it('should return the same string', function (done) {
+
+        var newTag = uncache.processTag('<script id="uncache"></script>');
+        assert.equal(newTag, '<script id="uncache"></script>');
+        done();
+    });
+});
+
+describe('parsing invalid input', function () {
+    it("should return false while calculateFileHash because file doesn't exist", function (done) {
+        var config = {
+            srcDir: './really-invalid-path'
+        };
+        var hash = uncache.calculateFileHash('this-is-invalid-too.js', config);
+        assert.equal(hash, false);
+        done();
+    });
+
+    it("should return empy object when there is no inline config", function (done) {
+        assert.equal(JSON.stringify({}), JSON.stringify(uncache.parseConfig(undefined)));
+        done();
+    });
+});
+
+
+
+describe('mkdir recursive', function () {
+    it('should create non-existent path', function (done) {
+        var fileName = '.tmp-uncache-test-mkdirRecursive';
+        if(fs.existsSync(fileName)) {
+            fs.rmdirSync(fileName);
+        }
+        uncache.mkdirRecursive('.tmp-uncache-test-mkdirRecursive');
+
+        fs.readdir(path.normalize('./'), function(err, files) {
+            assert.equal(files.indexOf(fileName) !== -1, true);
+
+            fs.rmdirSync(fileName);
+            done();
+        });
+    });
+});
 
 describe('uncache', function () {
     it('should parse ', function (done) {
--- uncache.js
+++ uncache.js
@@ -27,175 +27,171 @@ var util = require('util'),
     },
     globalConfig = {};
 
+var uncache = {
+    extractBlocks: function (html) {
+        var blocks = html.split(/<!--\s*enduncache\s*-->/);
+        var output = [];
+
+        blocks.forEach(function (block) {
+            var pieces = block.split(/<!--\s*uncache\s*(\(.*\))?\s*-->/);
+            output.push({type: 'html', content: pieces[0]});
+
+            if (typeof pieces[2] !== 'undefined') {
+                output.push({type: 'uncache', config: pieces[1], content: pieces[2]})
+            }
+        });
+        return output;
+    },
+
+    mkdirRecursive: function (dir) {
+        var dirs,
+            i,
+            _path;
+
+        if (fs.existsSync(path.normalize(dir))) {
+            return false;
+        }
+
+        dirs = path.normalize(dir).split(path.sep);
+
+        for (i = 0; i < dirs.length; i += 1) {
+            _path = path.normalize(dirs.slice(0, i + 1).join('/'));
+            fs.existsSync(_path) || fs.mkdirSync(_path);
+        }
+    },
+
+    processBlock: function (config, content) {
+        var output = '';
+        var tags = uncache.extractTags(content);
+
+        globalConfig = util._extend(globalConfig, uncache.parseConfig(config));
+
+        tags.forEach(function (link) {
+            output += uncache.processTag(link);
+            output += '\r\n';
+        });
+        output = output.slice(0, -2);
+        return output;
+    },
+    copyFile: function (fileName, newFileName, config) {
+        var sourceFile = path.join(config.srcDir, fileName);
+        var distFile = path.join(config.distDir, newFileName);
+
+        uncache.mkdirRecursive(path.dirname(distFile));
+        fs.createReadStream(sourceFile).pipe(fs.createWriteStream(distFile));
+    },
+    processTag: function (tag) {
+        var regexp,
+            fileName,
+            newFileName,
+            newTag;
+        if (tag.indexOf('src=') > 0) {
+            regexp = /<.*\s+src=['"]([^'"]+)['"].*>/;
+        } else if (tag.indexOf('href=') > 0) {
+            regexp = /<.*\s+href=['"]([^'"]+)['"].*>/;
+        } else {
+            return tag;
+        }
+        fileName = tag.split(regexp)[1];
+        newFileName = uncache.getNewFileName(fileName, globalConfig);
+
+        if (globalConfig.rename) {
+            uncache.copyFile(fileName, newFileName, globalConfig);
+        }
+
+        newTag = tag.replace(fileName, newFileName);
+
+        return newTag;
+    },
+    getNewFileName: function (fileName, config) {
+
+        var append,
+            template;
+        if (config.append === 'hash') {
+            append = uncache.calculateFileHash(fileName, config);
+        } else if (config.append === 'time') {
+            append = Math.round(+new Date() / 1000);
+        } else {
+            append = config.append;
+        }
+
+        var extension = path.extname(fileName);
+        var dirName = path.dirname(fileName);
+        dirName = dirName === '.' ? '' : dirName;
+        var basename = path.basename(fileName, extension);
+
+
+        if (config.rename) {
+            template = hogan.compile(config.template);
+            return template.render({
+                name: basename,
+                extension: extension.substr(1),
+                path: dirName ? dirName + '/' : dirName,
+                append: append
+            });
+        } else {
+            return fileName + '?' + append;
+        }
+    },
+    calculateFileHash: function (fileName, config) {
+        var filePath = path.join(config.srcDir, fileName);
+        var data;
+
+        try {
+            data = fs.readFileSync(filePath);
+        } catch (e) {
+            console.error(e);
+            return false;
+        }
+        return md5(data.toString()).substr(0, 10);
+    },
+    extractTags: function (content) {
+        return content.match(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>|<link [^>]*>/gm);
+    },
+    parseConfig: function (configString) {
+        var config = {},
+            parts;
+
+        if (!configString) {
+            return {};
+        }
+
+        configString.substr(1, configString.length - 2).split(/\s*,\s*/).forEach(function (element) {
+            parts = element.split(':');
+            config[parts[0].trim()] = parts[1].trim();
+        });
+
+        //parse string true/false to bool
+        config.rename = config.rename === 'true';
+
+        return config;
+    },
+
+    /**
+     * @param html {string} HTML file content
+     * @param config {object} Config object
+     * @return {String} New HTML file content
+     * @api public
+     */
+    init: function (html, config) {
+        globalConfig = util._extend(defaultConfig, config);
+        var output = '';
+
+
+        uncache.extractBlocks(html).forEach(function (block) {
+            if (block.type === 'html') {
+                output += block.content;
+            } else {
+                output += uncache.processBlock(block.config, block.content);
+            }
+        });
+        return output;
+    }
+};
+
+
 /**
  * Module exports
  */
 
-module.exports = unCache;
-
-function extractBlocks(html) {
-    var blocks = html.split(/<!--\s*enduncache\s*-->/);
-    var output = [];
-
-    blocks.forEach(function (block) {
-        var pieces = block.split(/<!--\s*uncache\s*(\(.*\))?\s*-->/);
-        output.push({type: 'html', content: pieces[0]});
-
-        if (typeof pieces[2] !== 'undefined') {
-            output.push({type: 'uncache', config: pieces[1], content: pieces[2]})
-        }
-    });
-    return output;
-}
-
-function mkdirRecursive(dir) {
-    var dirs,
-        i,
-        _path;
-
-    if (fs.existsSync(path.normalize(dir))) {
-        return false;
-    }
-
-    dirs = path.normalize(dir).split(path.sep);
-
-    for (i = 0; i < dirs.length; i += 1) {
-        _path = path.normalize(dirs.slice(0, i + 1).join('/'));
-        if (!fs.existsSync(_path)) {
-            fs.mkdirSync(_path);
-        }
-    }
-}
-
-function processBlock(config, content) {
-    var output = '';
-    var tags = extractTags(content);
-
-    globalConfig = util._extend(globalConfig, parseConfig(config));
-
-    tags.forEach(function(link) {
-        output += processTag(link);
-        output += '\r\n';
-    });
-    output = output.slice(0, -2);
-    return output;
-}
-
-function copyFile(fileName, newFileName, config) {
-    var sourceFile = path.join(config.srcDir, fileName);
-    var distFile = path.join(config.distDir, newFileName);
-
-    mkdirRecursive(path.dirname(distFile));
-    fs.createReadStream(sourceFile).pipe(fs.createWriteStream(distFile));
-}
-
-function processTag(tag) {
-    var regexp,
-        fileName,
-        newFileName,
-        newTag;
-    if (tag.indexOf('src=') > 0) {
-        regexp = /<.*\s+src=['"]([^'"]+)['"].*>/;
-    } else if (tag.indexOf('href=') > 0) {
-        regexp = /<.*\s+href=['"]([^'"]+)['"].*>/;
-    } else {
-        return tag;
-    }
-    fileName = tag.split(regexp)[1];
-    newFileName = getNewFileName(fileName, globalConfig);
-
-    if(globalConfig.rename) {
-        copyFile(fileName, newFileName, globalConfig);
-    }
-
-    newTag = tag.replace(fileName, newFileName);
-
-    return newTag;
-}
-
-function getNewFileName(fileName, config) {
-
-    var append,
-        template;
-    if(config.append === 'hash') {
-        append = calculateFileHash(fileName, config);
-    } else if(config.append === 'time') {
-        append = Math.round(+new Date()/1000);
-    } else {
-        append = config.append;
-    }
-
-    var extension= path.extname(fileName);
-    var dirName = path.dirname(fileName);
-    dirName = dirName === '.' ? '' : dirName;
-    var basename = path.basename(fileName, extension);
-    template = hogan.compile(config.template);
-
-    if(config.rename) {
-        return template.render({
-            name: basename,
-            extension: extension.substr(1),
-            path: dirName ? dirName + '/' : dirName,
-            append: append
-        });
-    } else {
-        return fileName + '?' + append;
-    }
-}
-
-function calculateFileHash(fileName, config) {
-    var filePath = path.join(config.srcDir, fileName);
-    var data;
-
-    try {
-        data = fs.readFileSync(filePath);
-    } catch (e) {
-        console.error(e);
-        return false;
-    }
-    return md5(data.toString()).substr(0, 10);
-}
-
-function extractTags(content) {
-    return content.match(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>|<link [^>]*>/gm);
-}
-
-function parseConfig(configString) {
-    var config = {},
-        parts;
-
-    if(!configString) {
-        return {};
-    }
-
-    configString.substr(1, configString.length - 2).split(/\s*,\s*/).forEach(function(element) {
-        parts = element.split(':');
-        config[parts[0].trim()] = parts[1].trim();
-    });
-
-    //parse string true/false to bool
-    config.rename = config.rename === 'true';
-
-    return config;
-}
-
-/**
- * @param html {string} HTML file content
- * @param config {object} Config object
- * @return {String} New HTML file content
- * @api public
- */
-function unCache(html, config) {
-    globalConfig = util._extend(defaultConfig, config);
-    var output = '';
-
-
-    extractBlocks(html).forEach(function (block) {
-        if (block.type === 'html') {
-            output += block.content;
-        } else if (block.type === 'uncache') {
-            output += processBlock(block.config, block.content);
-        }
-    });
-    return output;
-}
+module.exports = uncache;
